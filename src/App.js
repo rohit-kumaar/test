9874c09e969145ed9623f413402f76290e90b529
Rohit Kumar <94822153+rohitkumarneosoft@users.noreply.github.com>
2023-04-06 22:44:53 +0530

    final : meter

--- src/App.js
+++ src/App.js
@@ -1,27 +1,105 @@
 import "./App.css";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Home from "./Home";
-import Login from "./Login";
-import Register from "./Register";
-import { ToastContainer } from "react-toastify";
-import Appheader from "./Appheader";
-import Customer from "./Customer";
+import { Chart } from "chart.js";
+import ChartDataLabels from "chartjs-plugin-datalabels";
+import "chartjs-plugin-labels";
+import * as Gauge from "chartjs-gauge";
+import { useEffect } from "react";
 
-function App() {
+var data = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100];
+var value = 68;
+
+var config = {
+  type: "gauge",
+  data: {
+    labels: ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"],
+    datasets: [
+      {
+        label: "Current Appeal Risk",
+        data: data,
+        value: value,
+        minValue: 0,
+        backgroundColor: [
+          "#2470a0",
+          "#3AF4D5",
+          "#3AF4D5",
+          "#00a03e",
+          "#00a03e",
+          "#00a03e",
+          "#FFE74C",
+          "#FFE74C",
+          "#dd5f32",
+          "#e74700",
+        ],
+        borderWidth: 2,
+      },
+    ],
+  },
+  options: {
+    legend: {
+      display: true,
+      position: "bottom",
+      labels: {
+        generateLabels: {},
+      },
+    },
+    responsive: true,
+    title: {
+      display: true,
+      text: "Financial Risk",
+    },
+    layout: {
+      padding: {
+        bottom: 20,
+      },
+    },
+    needle: {
+      radiusPercentage: 1,
+      widthPercentage: 1,
+      lengthPercentage: 60,
+      color: "rgba(0, 0, 0, 1)",
+    },
+    valueLabel: {
+      fontSize: 12,
+      formatter: function (value, context) {
+        return value + "X";
+      },
+    },
+    plugins: {
+      datalabels: {
+        display: "auto",
+        formatter: function (value, context) {
+          return context.chart.data.labels[context.dataIndex];
+        },
+        color: function (context) {
+          return "white";
+        },
+
+        font: function (context) {
+          var innerRadius = Math.round(context.chart.innerRadius);
+          console.log(innerRadius);
+          var size = Math.round(innerRadius / 8);
+
+          return {
+            weight: "normal",
+            size: size,
+          };
+        },
+      },
+    },
+  },
+};
+
+export default function App() {
+  useEffect(() => {
+    var ctx = document.getElementById("myChart").getContext("2d");
+    const myGauge = new Chart(ctx, config);
+    myGauge.update();
+    const val = setTimeout(() => console.log(myGauge.toBase64Image()), 800);
+  }, []);
+
   return (
-    <div className="App">
-      <ToastContainer theme="colored" position="top-center"></ToastContainer>
-      <BrowserRouter>
-        <Appheader></Appheader>
-        <Routes>
-          <Route path="/" element={<Home />}></Route>
-          <Route path="/login" element={<Login />}></Route>
-          <Route path="/register" element={<Register />}></Route>
-          <Route path="/customer" element={<Customer />}></Route>
-        </Routes>
-      </BrowserRouter>
+    <div className="meter">
+      <canvas id="myChart"></canvas>
     </div>
   );
 }
-
-export default App;
